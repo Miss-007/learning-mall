8fdc7fa37a9323d5afc6cdb9c306cfbfb40e6f43
qingqh <474421820@qq.com>
2021-11-05 23:40:14 +0800

    the first step

---
+++ src/test/java/com/huang/mallpractice/MallPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.mallpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
